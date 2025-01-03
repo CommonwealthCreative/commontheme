b2d1f3f980dc1b292621485d4603199d8c2414ed
Commonwealth Creative <hello.commonwealthcreative@gmail.com>
2025-01-03 11:56:31 -0500

    Header, Index & Footer - 2025

--- footer.php
+++ footer.php
@@ -11,41 +11,40 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-  <section class="cta">
+<section class="cta">
     <div class="_1300">
-      <div id="w-node-d7a95ab3-27c4-33b9-6364-0b87ed4619e5-ed4619e3" class="w-layout-layout footer-cta-stack wf-layout-layout">
-        <div id="w-node-d7a95ab3-27c4-33b9-6364-0b87ed4619e6-ed4619e3" class="w-layout-cell">
-          <div data-w-id="d7a95ab3-27c4-33b9-6364-0b87ed4619e7" class="common-card backgrounddark ctafooter">
-            <div id="w-node-d7a95ab3-27c4-33b9-6364-0b87ed4619e8-ed4619e3" class="w-layout-layout fortystack wf-layout-layout">
-              <div id="w-node-d7a95ab3-27c4-33b9-6364-0b87ed4619e9-ed4619e3" class="w-layout-cell">
-                <p class="mid">The catalyst for your marketing or engineering team. <span class="lite">Start Today.</span></p>
+      <div id="w-node-_4fc241e5-049e-8ddf-69d9-57401d262189-1d262188" class="w-layout-layout commonstack wf-layout-layout">
+        <div id="w-node-_4fc241e5-049e-8ddf-69d9-57401d26218a-1d262188" class="w-layout-cell">
+          <div data-w-id="4fc241e5-049e-8ddf-69d9-57401d26218b" class="commoncard backgrounddark ctafooter">
+            <div id="w-node-_4fc241e5-049e-8ddf-69d9-57401d26218c-1d262188" class="w-layout-layout commonstack wf-layout-layout">
+              <div id="w-node-_4fc241e5-049e-8ddf-69d9-57401d26218d-1d262188" class="w-layout-cell">
+                <h3 class="textlite mid">The catalyst for your marketing or engineering team. <span class="textwhite">Start Today.</span></h3>
+                <a href="shop.html" target="_blank" class="button backgroundwhite w-button">Plans &amp; Pricing</a>
               </div>
-              <div id="w-node-d7a95ab3-27c4-33b9-6364-0b87ed4619ee-ed4619e3" class="w-layout-cell landscapehide"></div>
+              <div id="w-node-_4fc241e5-049e-8ddf-69d9-57401d262194-1d262188" class="w-layout-cell landscapehide"></div>
             </div>
-            <a href="/#pricing" target="_blank" class="button backgroundwhite w-button">Plans &amp; Pricing</a>
             <div class="ctabottom">
-              <a data-w-id="d7a95ab3-27c4-33b9-6364-0b87ed4619f2" href="/#process" class="actionlink textwhite w-inline-block">
+              <a href="/strategies-methods/" target="_blank" class="actionlink w-inline-block">
                 <div class="actionpulse backgroundgreen"></div>
-                <div class="fontawesolid textwhite"></div>
-                <p>Our Process &amp; Capabilities</p>
+                <div class="fontawesolid"></div>
+                <p class="iconlinktext textwhite">Our Services</p>
               </a>
             </div>
           </div>
         </div>
-        <div id="w-node-d7a95ab3-27c4-33b9-6364-0b87ed4619f8-ed4619e3" class="w-layout-cell">
-          <div data-w-id="d7a95ab3-27c4-33b9-6364-0b87ed4619f9" class="common-card backgroundwhite ctafooter">
-            <div id="w-node-d7a95ab3-27c4-33b9-6364-0b87ed4619fa-ed4619e3" class="w-layout-layout fortystack wf-layout-layout">
-              <div id="w-node-d7a95ab3-27c4-33b9-6364-0b87ed4619fb-ed4619e3" class="w-layout-cell">
-                <p class="mid">Enterprise technology and design.<span class="textlite"> Delivered on your timeline.</span></p>
+        <div id="w-node-_4fc241e5-049e-8ddf-69d9-57401d26219c-1d262188" class="w-layout-cell">
+          <div data-w-id="4fc241e5-049e-8ddf-69d9-57401d26219d" class="commoncard backgroundwhite ctafooter">
+            <div id="w-node-_4fc241e5-049e-8ddf-69d9-57401d26219e-1d262188" class="w-layout-layout commonstack wf-layout-layout">
+              <div id="w-node-_4fc241e5-049e-8ddf-69d9-57401d26219f-1d262188" class="w-layout-cell">
+                <h3 class="mid">Enterprise design and technology.<span class="textlite"> Delivered on your timeline.</span></h3>
+                <a href="#cal" target="_blank" class="button backgrounddark w-button">Book An Introduction</a>
               </div>
-              <div id="w-node-d7a95ab3-27c4-33b9-6364-0b87ed461a00-ed4619e3" class="w-layout-cell landscapehide"></div>
+              <div id="w-node-_4fc241e5-049e-8ddf-69d9-57401d2621a6-1d262188" class="w-layout-cell landscapehide"></div>
             </div>
-            <a href="#" data-cal-link="hello.mattsmall/15min" data-cal-namespace="" data-cal-config='{"layout":"month_view"}' target="_blank" class="button backgrounddark w-button">Book An Introduction</a>
             <div class="ctabottom">
-              <a data-w-id="d7a95ab3-27c4-33b9-6364-0b87ed461a04" href="https://thecommonwealthcreative.com/contact/" class="actionlink w-inline-block">
-                <div class="fontawesolid iconrotate"></div>
-                <p>Start Discovery</p>
+              <a data-w-id="4fc241e5-049e-8ddf-69d9-57401d2621a8" href="contact.html" class="icontext w-inline-block">
+                <div class="fontaw rocket"></div>
+                <p class="iconlinktext">Start Here</p>
               </a>
             </div>
           </div>
@@ -53,37 +52,41 @@
       </div>
     </div>
   </section>
-<section id="footer" class="footer">
+  <section id="footer" class="footer">
     <div class="_1300">
-      <div id="w-node-_616fb841-1116-4d08-6521-6f79d03baa37-d03baa35" class="w-layout-layout footerstack wf-layout-layout">
-        <div id="w-node-_616fb841-1116-4d08-6521-6f79d03baa38-d03baa35" class="w-layout-cell">
-          <a data-w-id="616fb841-1116-4d08-6521-6f79d03baa39" href="#" data-cal-link="hello.mattsmall/15min" data-cal-namespace="" data-cal-config='{"layout":"month_view"}' class="actionlink textwhite w-inline-block">
-            <div class="fontawesolid iconrotate"></div>
-            <p class="heavy footerlinks">Schedule An Introduction</p>
+      <div id="w-node-_274747c0-b1a7-474d-88ef-827ff3758576-f3758574" class="w-layout-layout footerstack wf-layout-layout">
+        <div id="w-node-_274747c0-b1a7-474d-88ef-827ff3758577-f3758574" class="w-layout-cell">
+          <a data-w-id="274747c0-b1a7-474d-88ef-827ff3758578" href="#cal" class="icontext textwhite w-inline-block">
+            <div class="fontaw rocket"></div>
+            <p class="iconlinktext">Schedule An Intro<span class="landscapehide">duction</span></p>
           </a>
-          <a data-w-id="616fb841-1116-4d08-6521-6f79d03baa3e" href="mailto:hi@thecommonwealthcreative.com" class="actionlink textwhite w-inline-block">
-            <div class="fontawesolid iconrotate"></div>
-            <p class="heavy footerlinks">hi@thecommonwealthcreative.com</p>
+          <a data-w-id="274747c0-b1a7-474d-88ef-827ff375857f" href="mailto:hi@thecommonwealthcreative.com" class="icontext textwhite landscapeshow w-inline-block">
+            <div class="fontaw rocket"></div>
+            <p class="iconlinktext">Email Us</p>
           </a>
-          <a data-w-id="616fb841-1116-4d08-6521-6f79d03baa43" href="tel:+18-44241348" class="actionlink textwhite w-inline-block">
-            <div class="fontawesolid iconrotate"></div>
-            <p class="heavy footerlinks">804-424-1348</p>
+          <a data-w-id="274747c0-b1a7-474d-88ef-827ff3758584" href="mailto:hi@thecommonwealthcreative.com" class="icontext textwhite landscapehide w-inline-block">
+            <div class="fontaw rocket"></div>
+            <p class="iconlinktext">hi@thecommonwealthcreative.com</p>
+          </a>
+          <a data-w-id="274747c0-b1a7-474d-88ef-827ff3758589" href="tel:+18044241348" class="icontext textwhite w-inline-block">
+            <div class="fontaw rocket"></div>
+            <p class="iconlinktext">804-424-1348</p>
           </a>
         </div>
-        <div id="w-node-_616fb841-1116-4d08-6521-6f79d03baa48-d03baa35" class="w-layout-cell right flexblockcentercenter">
+        <div id="w-node-_274747c0-b1a7-474d-88ef-827ff375858e-f3758574" class="w-layout-cell right flexblockcentercenter">
           <div class="footerlinks">
-            <a href="https://www.adobe.com/creativecloud.html" target="_blank" class="footerlogo w-inline-block"><img width="56" loading="lazy" alt="" src="/wp-content/themes/commontheme/images/commonwealth-creative-adobe.svg" class="affiliate"></a>
-            <a href="https://automattic.pxf.io/commonwealth" target="_blank" class="footerlogo w-inline-block"><img width="60" loading="lazy" alt="" src="/wp-content/themes/commontheme/images/commonwealth-creative-wordpress.svg" class="affiliate"></a>
-            <a href="https://www.figma.com/" target="_blank" class="footerlogo w-inline-block"><img width="39" loading="lazy" alt="" src="/wp-content/themes/commontheme/images/commonwealth-creative-figma.svg" class="affiliate"></a>
-            <a href="https://try.webflow.com/k2wvsi66bv57" target="_blank" class="footerlogo w-inline-block"><img width="75" loading="lazy" alt="" src="/wp-content/themes/commontheme/images/commonwealth-creative-webflow.svg" class="affiliate"></a>
-            <a href="https://wpengine.com/partnerspecialoffer/?w_agcid=jecEfwKu" target="_blank" class="footerlogo w-inline-block"><img width="50" loading="lazy" alt="" src="/wp-content/themes/commontheme/images/commonwealth-creative-wpengine.svg" class="affiliate"></a>
+            <a href="https://www.adobe.com/creativecloud.html" target="_blank" class="footerlogo w-inline-block"><img width="56" loading="lazy" alt="" src="wp-content/themes/commontheme2025/images/commonwealth-creative-adobe.svg" class="affiliate"></a>
+            <a href="https://automattic.pxf.io/commonwealth" target="_blank" class="footerlogo w-inline-block"><img width="60" loading="lazy" alt="" src="wp-content/themes/commontheme2025/images/commonwealth-creative-wordpress.svg" class="affiliate"></a>
+            <a href="https://www.figma.com/" target="_blank" class="footerlogo w-inline-block"><img width="39" loading="lazy" alt="" src="wp-content/themes/commontheme2025/images/commonwealth-creative-figma.svg" class="affiliate"></a>
+            <a href="https://try.webflow.com/k2wvsi66bv57" target="_blank" class="footerlogo w-inline-block"><img width="75" loading="lazy" alt="" src="wp-content/themes/commontheme2025/images/commonwealth-creative-webflow.svg" class="affiliate"></a>
+            <a href="https://wpengine.com/partnerspecialoffer/?w_agcid=jecEfwKu" target="_blank" class="footerlogo w-inline-block"><img width="50" loading="lazy" alt="" src="wp-content/themes/commontheme2025/images/commonwealth-creative-wpengine.svg" class="affiliate"></a>
           </div>
         </div>
       </div>
-      <div id="w-node-_616fb841-1116-4d08-6521-6f79d03baa54-d03baa35" class="w-layout-layout footerinfostack wf-layout-layout">
-        <div id="w-node-_616fb841-1116-4d08-6521-6f79d03baa55-d03baa35" class="w-layout-cell withlove">
+      <div id="w-node-_274747c0-b1a7-474d-88ef-827ff375859a-f3758574" class="w-layout-layout footerinfostack wf-layout-layout">
+        <div id="w-node-_274747c0-b1a7-474d-88ef-827ff375859b-f3758574" class="w-layout-cell withlove centered">
           <div class="w-layout-hflex">
-            <div class="withlove">
+            <div class="withlove textwhite">
               <div class="w-layout-hflex flex-block">
                 <div>Made with</div>
                 <div class="fontawesolid greenheart"></div>
@@ -92,28 +95,63 @@
             </div>
           </div>
         </div>
-        <div id="w-node-_616fb841-1116-4d08-6521-6f79d03baa5f-d03baa35" class="w-layout-cell cell">
+        <div id="w-node-_274747c0-b1a7-474d-88ef-827ff37585a5-f3758574" class="w-layout-cell cell">
           <div class="iconlinks footericons">
-            <div class="fontawebrands">
-              <a href="https://www.linkedin.com/company/hi-commonwealth" target="_blank" class="iconlinks textwhite"></a>
+            <div>
+              <a href="https://www.linkedin.com/company/hi-commonwealth" target="_blank" class="fontawebrands textwhite"></a>
             </div>
-            <div class="fontawebrands">
-              <a href="https://twitter.com/hi_commonwealth" target="_blank" class="iconlinks textwhite"></a>
+            <div>
+              <a href="https://twitter.com/hi_commonwealth" target="_blank" class="fontawebrands textwhite"></a>
             </div>
-            <div class="fontawebrands">
-              <a href="https://www.instagram.com/hi_commonwealth/" target="_blank" class="iconlinks textwhite"></a>
+            <div>
+              <a href="https://www.instagram.com/hi_commonwealth/" target="_blank" class="fontawebrands textwhite"></a>
             </div>
-            <div class="fontawebrands">
-              <a href="https://github.com/CommonwealthCreative" target="_blank" class="iconlinks textwhite"></a>
+            <div>
+              <a href="https://github.com/CommonwealthCreative" target="_blank" class="fontawebrands textwhite"></a>
             </div>
           </div>
         </div>
       </div>
     </div>
-  
+    <div class="w-embed w-script"><!--  Cal element-click embed code begins  -->
+      <script type="text/javascript">
+  (function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; if(typeof namespace === "string"){cal.ns[namespace] = cal.ns[namespace] || api;p(cal.ns[namespace], ar);p(cal, ["initNamespace", namespace]);} else p(cal, ar); return;} p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
+Cal("init", "15min", {origin:"https://cal.com"});
+  // Important: Please add the following attributes to the element that should trigger the calendar to open upon clicking.
+  // `data-cal-link="hello.mattsmall/15min"`
+  // data-cal-namespace="15min"
+  // `data-cal-config='{"layout":"month_view"}'`
+  Cal.ns["15min"]("ui", {"styles":{"branding":{"brandColor":"#000000"}},"hideEventTypeDetails":false,"layout":"month_view"});
+  </script>
+      <!--  Cal element-click embed code ends  -->
+    </div>
+    <div class="w-embed">
+      <style>
+.text-holder {
+-webkit-clip-path: inset(0px 0px 0px 0px);
+clip-path: inset(0px 0px 0px 0px);
+}
+.text {transform: translateZ(0);}
+@media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
+   /* IE10+ */
+.text-holder {clip: rect(0,auto,auto,0);}
+.workcontent {display: block; width: 100%; text-align: center; vertical-align: middle; padding-top: 50vh;} 
+}
+@supports (-ms-accelerator:true) {
+   /* Edge 12+ */ 
+.text-holder {clip: rect(0,auto,auto,0);}
+.workcontent {display: block; width: 100%;  text-align: center; vertical-align: middle; padding-top: 50vh;} 
+}
+@supports (-ms-ime-align:auto) {
+    /* Edge 16+ */ 
+.text-holder {clip: rect(0,auto,auto,0);}
+.workcontent {display: block; width: 100%;  text-align: center; vertical-align: middle; padding-top: 50vh;} 
+}
+</style>
+    </div>
   </section>
-</div>
-<script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=66daff0553e4462bf4ae1607" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=67640e6a4a1ce4ad68fc077b" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
 
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 
 <?php 
 
-/* Indexes */ if ( is_home() ) { echo 'data-wf-page="66daff0553e4462bf4ae160d"'; } 
+/* Indexes */ if ( is_home() ) { echo 'data-wf-page="67640e6a4a1ce4ad68fc0789"'; } 
             if ( is_page_template( 'work.php') ) { echo 'data-wf-page="66dd4a19079776ced5a5c9d1"'; } 
             if ( is_page_template( 'thecodex.php') ) { echo 'data-wf-page="66dd4a19079776ced5a5c9d1"'; } 
             if ( is_page_template( 'about.php') ) { echo 'data-wf-page="66dd5a138058bad06fe7abce"'; } 
@@ -33,7 +33,7 @@
       
       ?>  
 
-data-wf-site="66daff0553e4462bf4ae1607" >
+data-wf-site="67640e6a4a1ce4ad68fc077b" >
 
 
 <head>
@@ -66,43 +66,43 @@ Cal("init",  {origin:"https://cal.com"});
 <body <?php body_class(); ?>>
   
 <div class="nav">
-    <div data-w-id="3ca514ae-0c9e-bd3f-9611-c95457eb3c23" class="menubg">
-      <div class="menu-left _1300">
-        <a data-w-id="3ca514ae-0c9e-bd3f-9611-c95457eb3c25" href="<?php echo home_url(); ?>" class="brand herologo w-nav-brand"></a>
+    <div data-w-id="71de8595-15d3-a0ee-aa62-ad8501dcfdca" class="menubg">
+      <div class="menu-left">
+        <a href="index.html" aria-current="page" class="brand w-nav-brand w--current"></a>
         <div class="w-layout-hflex navflex">
-          <a data-w-id="3ca514ae-0c9e-bd3f-9611-c95457eb3c27" href="https://thecommonwealthcreative.com/#pricing" class="showreel w-inline-block">
+          <a data-w-id="71de8595-15d3-a0ee-aa62-ad8501dcfdce" href="contact.html" class="icontext w-inline-block">
             <div class="fontaw rocket"></div>
-            <p class="showreel-title project">Start Here</p>
+            <p class="iconlinktext textwhite">Start Here</p>
           </a>
-          <div data-w-id="3ca514ae-0c9e-bd3f-9611-c95457eb3c2c" data-is-ix2-target="1" class="menu" data-animation-type="lottie" data-src="https://uploads-ssl.webflow.com/5eaa9208297df21101de4911/5eaa9208297df28508de494b_lf30_editor_XLPk4g.json" data-loop="0" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="4" data-duration="0"></div>
+          <div data-w-id="71de8595-15d3-a0ee-aa62-ad8501dcfdd3" data-is-ix2-target="1" class="menu" data-animation-type="lottie" data-src="https://uploads-ssl.webflow.com/5eaa9208297df21101de4911/5eaa9208297df28508de494b_lf30_editor_XLPk4g.json" data-loop="0" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="4" data-duration="0"></div>
         </div>
       </div>
     </div>
-    <div class="overlay">
-      <div id="w-node-_3ca514ae-0c9e-bd3f-9611-c95457eb3c2e-57eb3c22" class="menu-links">
-        <a data-w-id="3ca514ae-0c9e-bd3f-9611-c95457eb3c3b" href="/" class="heading navoverlayheading">Home</a>
-        <a data-w-id="3ca514ae-0c9e-bd3f-9611-c95457eb3c3d" href="/strategies-methods/" class="heading navoverlayheading">Services</a>
-        <a data-w-id="3ca514ae-0c9e-bd3f-9611-c95457eb3c41" href="/#pricing" class="heading navoverlayheading">Pricing</a>
-        <a data-w-id="3ca514ae-0c9e-bd3f-9611-c95457eb3c3f" href="/work" class="heading navoverlayheading">Our Work</a>
-        <a data-w-id="3ca514ae-0c9e-bd3f-9611-c95457eb3c41" href="/shop" class="heading navoverlayheading">Shop</a>
-        <a data-w-id="3ca514ae-0c9e-bd3f-9611-c95457eb3c41" href="https://thecommonwealthcreative.com/about" class="heading navoverlayheading">About</a>
-        <a data-w-id="3ca514ae-0c9e-bd3f-9611-c95457eb3c43" href="#" data-cal-link="hello.mattsmall/15min" data-cal-namespace="" data-cal-config='{"layout":"month_view"}' target="_blank" class="actionlink textwhite w-inline-block" style="margin-top:20px; margin-bottom:20px;">
+    <div class="menuoverlay">
+      <div id="w-node-_71de8595-15d3-a0ee-aa62-ad8501dcfdd5-01dcfdc9" class="menucontainer">
+        <a href="/about-commonwealth-creative" data-w-id="71de8595-15d3-a0ee-aa62-ad8501dcfdd6" class="paytone slidelink">About.</a>
+        <a data-w-id="71de8595-15d3-a0ee-aa62-ad8501dcfdd8" href="/strategies-methods/" class="paytone slidelink">Services.</a>
+        <a data-w-id="71de8595-15d3-a0ee-aa62-ad8501dcfdda" href="work.html" class="paytone slidelink">Our Work.</a>
+        <a data-w-id="0aab7bf9-f048-7c2a-dbd8-dcf767e86630" href="codex.html" class="paytone slidelink">Codex.</a>
+        <a data-w-id="71de8595-15d3-a0ee-aa62-ad8501dcfddc" href="shop.html" class="paytone slidelink">Shop.</a>
+        <a data-w-id="71de8595-15d3-a0ee-aa62-ad8501dcfdde" href="contact.html" class="paytone slidelink">Contact.</a>
+        <a href="#cal" target="_blank" class="actionlink w-inline-block">
           <div class="actionpulse backgroundgreen"></div>
-          <div class="fontawesolid rocket"></div>
-          <p>Book An Introduction</p>
+          <div class="fontawesolid rocket"></div>
+          <p class="iconlinktext">Schedule An Introduction</p>
         </a>
-        <div class="iconlinks menusocials">
-          <div class="fontawebrands">
-            <a href="https://www.linkedin.com/company/hi-commonwealth" target="_blank" class="iconlinks textwhite"></a>
+        <div class="iconlinks">
+          <div>
+            <a href="https://www.linkedin.com/company/hi-commonwealth" target="_blank" class="fontawebrands"></a>
           </div>
-          <div class="fontawebrands">
-            <a href="https://twitter.com/hi_commonwealth" target="_blank" class="iconlinks textwhite"></a>
+          <div>
+            <a href="https://twitter.com/hi_commonwealth" target="_blank" class="fontawebrands"></a>
           </div>
-          <div class="fontawebrands">
-            <a href="https://www.instagram.com/hi_commonwealth/" target="_blank" class="iconlinks textwhite"></a>
+          <div>
+            <a href="https://www.instagram.com/hi_commonwealth/" target="_blank" class="fontawebrands"></a>
           </div>
-          <div class="fontawebrands">
-            <a href="https://github.com/CommonwealthCreative" target="_blank" class="iconlinks textwhite"></a>
+          <div>
+            <a href="https://github.com/CommonwealthCreative" target="_blank" class="fontawebrands"></a>
           </div>
         </div>
       </div>
@@ -111,8 +111,29 @@ Cal("init",  {origin:"https://cal.com"});
         <a href="https://www.linkedin.com/company/the-commonwealth-creative/" target="_blank" class="social-links">Linkedin</a>
         <a href="https://twitter.com/hi_commonwealth" target="_blank" class="social-links">X (Twitter)</a>
         <a href="https://www.instagram.com/hi_commonwealth/" target="_blank" class="social-links">Instagram</a>
+        <a href="https://github.com/CommonwealthCreative" target="_blank" class="social-links">GitHub</a>
       </div>
-      <div class="menu-bg"></div>
+      <div class="menu-bg">
+        <div id="work" data-w-id="71de8595-15d3-a0ee-aa62-ad8501dcfdfc" class="w-layout-hflex carouselcontainertext textwhite">
+          <div class="featuredtextcarousel">
+            <div class="becauseiconfeatured">
+              <div>COMMONWEALTH</div>
+            </div>
+            <div class="becauseiconfeatured">
+              <div>CREATIVE</div>
+            </div>
+          </div>
+          <div class="featuredtextcarousel">
+            <div class="becauseiconfeatured">
+              <div>COMMONWEALTH</div>
+            </div>
+            <div class="becauseiconfeatured">
+              <div>CREATIVE</div>
+            </div>
+          </div>
+        </div>
+        <div data-w-id="71de8595-15d3-a0ee-aa62-ad8501dcfe0b" data-is-ix2-target="1" class="lottie-animation" data-animation-type="lottie" data-src="https://uploads-ssl.webflow.com/6518f97551279edd72aff36b/6518f97551279edd72aff37a_animation_ln5kc7yh.json" data-loop="0" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="5" data-duration="0"></div>
+      </div>
     </div>
   </div>
 <?php wp_body_open(); ?>
