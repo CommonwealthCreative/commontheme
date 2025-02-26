dbf40ba24534a11bdc3b64f2a2e153548bfa2ed0
Commonwealth Creative <hello.commonwealthcreative@gmail.com>
2025-02-26 17:40:43 -0500

    technologies

--- technologies.php
+++ technologies.php
@@ -13,46 +13,49 @@
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/figma.png" alt="" class="toolsimg">
           <div>Figma</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/shopify.png" alt="" class="toolsimg">
-          <div>Shopify</div>
-        </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/webflow.png" alt="" class="toolsimg">
-          <div>Webflow</div>
-        </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/restapit.png" alt="" class="toolsimg">
-          <div>Rest API</div>
-        </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/github-icon-2.svg" alt="" class="toolsimg">
-          <div>GItHub</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/adobe-creative-cloud6378.png" alt="" class="toolsimg">
+          <div>Creative Cloud</div>
         </a>
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/visualstudio.png" alt="" class="toolsimg">
           <div>Visual Studio</div>
         </a>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/typescript.svg" alt="" class="toolsimg">
+          <div>Typescript</div>
+        </a>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/react.svg" alt="" class="toolsimg">
+          <div>React</div>
+        </a>
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/wordpress.png" alt="" class="toolsimg">
           <div>WordPress</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/javascript.png" alt="" class="toolsimg">
-          <div>Javascript</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/webflow.png" alt="" class="toolsimg">
+          <div>Webflow</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/php.png" alt="" class="toolsimg">
-          <div>PHP</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/framer.svg" alt="" class="toolsimg">
+          <div>Framer</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/htmlcss.png" alt="" class="toolsimg">
-          <div>HTML/CSS</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/shopify.png" alt="" class="toolsimg">
+          <div>Shopify</div>
+        </a>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/docker.svg" alt="" class="toolsimg">
+          <div>Docker</div>
+        </a>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/github-icon-2.svg" alt="" class="toolsimg">
+          <div>GitHub</div>
         </a>
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/mySQL.png" alt="" class="toolsimg">
           <div>MySQL</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/adobe-creative-cloud6378.png" alt="" class="toolsimg">
-          <div>Creative Cloud</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/hubspot.svg" alt="" class="toolsimg">
+          <div>Hubspot</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/monday.png" alt="" class="toolsimg">
-          <div>Monday</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/mailchimp.svg" alt="" class="toolsimg">
+          <div>Mailchimp</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/13131.png" alt="" class="toolsimg">
-          <div>Basecamp</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/midjourney.svg" alt="" class="toolsimg">
+          <div>MidJourney</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/Asset-5.svg" alt="" class="toolsimg">
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/openai.svg" alt="" class="toolsimg">
           <div>OpenAI</div>
         </a>
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/screamingfrog.png" alt="" class="toolsimg">
@@ -67,13 +70,13 @@
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/powerautomate.png" alt="" class="toolsimg">
           <div>Power Automate</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/azure.png" alt="" class="toolsimg">
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/azure.svg" alt="" class="toolsimg">
           <div>Microsoft Azure</div>
         </a>
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/aws.png" alt="" class="toolsimg">
           <div>Amazon Web Server</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/Asset-13.svg" alt="" class="toolsimg">
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/linux.svg" alt="" class="toolsimg">
           <div>Linux Server</div>
         </a>
       </div>
@@ -81,46 +84,49 @@
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/figma.png" alt="" class="toolsimg">
           <div>Figma</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/shopify.png" alt="" class="toolsimg">
-          <div>Shopify</div>
-        </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/webflow.png" alt="" class="toolsimg">
-          <div>Webflow</div>
-        </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/restapit.png" alt="" class="toolsimg">
-          <div>Rest API</div>
-        </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/github.png" alt="" class="toolsimg">
-          <div>GItHub</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/adobe-creative-cloud6378.png" alt="" class="toolsimg">
+          <div>Creative Cloud</div>
         </a>
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/visualstudio.png" alt="" class="toolsimg">
           <div>Visual Studio</div>
         </a>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/typescript.svg" alt="" class="toolsimg">
+          <div>Typescript</div>
+        </a>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/react.svg" alt="" class="toolsimg">
+          <div>React</div>
+        </a>
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/wordpress.png" alt="" class="toolsimg">
           <div>WordPress</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/javascript.png" alt="" class="toolsimg">
-          <div>Javascript</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/webflow.png" alt="" class="toolsimg">
+          <div>Webflow</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/php.png" alt="" class="toolsimg">
-          <div>PHP</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/framer.svg" alt="" class="toolsimg">
+          <div>Framer</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/htmlcss.png" alt="" class="toolsimg">
-          <div>HTML/CSS</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/shopify.png" alt="" class="toolsimg">
+          <div>Shopify</div>
+        </a>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/docker.svg" alt="" class="toolsimg">
+          <div>Docker</div>
+        </a>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/github-icon-2.svg" alt="" class="toolsimg">
+          <div>GitHub</div>
         </a>
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/mySQL.png" alt="" class="toolsimg">
           <div>MySQL</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/adobe-creative-cloud6378.png" alt="" class="toolsimg">
-          <div>Creative Cloud</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/hubspot.svg" alt="" class="toolsimg">
+          <div>Hubspot</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/monday.png" alt="" class="toolsimg">
-          <div>Monday</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/mailchimp.svg" alt="" class="toolsimg">
+          <div>Mailchimp</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/13131.png" alt="" class="toolsimg">
-          <div>Basecamp</div>
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/midjourney.svg" alt="" class="toolsimg">
+          <div>MidJourney</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/Asset-5.svg" alt="" class="toolsimg">
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/openai.svg" alt="" class="toolsimg">
           <div>OpenAI</div>
         </a>
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/screamingfrog.png" alt="" class="toolsimg">
@@ -135,13 +141,13 @@
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/powerautomate.png" alt="" class="toolsimg">
           <div>Power Automate</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/azure.png" alt="" class="toolsimg">
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/azure.svg" alt="" class="toolsimg">
           <div>Microsoft Azure</div>
         </a>
         <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/aws.png" alt="" class="toolsimg">
           <div>Amazon Web Server</div>
         </a>
-        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/Asset-13.svg" alt="" class="toolsimg">
+        <a href="/the-codex" class="toolsicon w-inline-block"><img loading="lazy" src="/wp-content/themes/commontheme2025/images/linux.svg" alt="" class="toolsimg">
           <div>Linux Server</div>
         </a>
       </div>
